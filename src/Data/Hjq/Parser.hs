23e56f3530d7c188cccc7d69b479140599fadb9e
Fumihito Morita <fumihito.morita@jollygood.co.jp>
2020-09-11 05:20:48 +0900

    p317

--- src/Data/Hjq/Parser.hs
+++ src/Data/Hjq/Parser.hs
@@ -8,4 +8,24 @@ data JqFilter
     deriving (Show, Read, Eq)
 
 parseJqFilter :: Text -> Either Text JqFilter
-parseJqFilter s = undefined
+parseJqFilter s = showParseResult
+    $ parse (jqFilterParser <* endOfInput) s `feed` ""
+
+jqFilterParser :: Parser JqFilter
+jqFilterParser = char '.' >> (JqField <|> JqIndex <|> pure JqNil)
+    where
+        jqFilter :: Parser JqFilter
+        jqFilter = (char '.' >> JqField) <|> JqIndex <|> JqNil
+
+        jqField :: Parser JqFilter
+        jqField = JqField <$> word <*> jqFilter
+
+        jqIndex :: Parser JqFilter
+        jqIndex = JqIndex <$> (char '[' *> decimal <* '[') <*> jqFilter
+
+showParseResult :: Show a => Result a -> Either Text a
+showParseResult (Done _ r) = Right r
+showParseResult r = Left . pack $ show r
+
+word :: Parser Text
+word = fmap pack $ many1 (letter <|> char '-' <|> char '_' <|> digit)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,7 @@ main :: IO ()
 main = do
     runTestTT $ TestList
         [ jqFilterParserTest 
-      
+        , jqFilterParserSpacesTest
         ]
     return ()
 
@@ -18,4 +18,13 @@ jqFilterParserTest = TestList
     , "jqFilterParser test 3" ~: Hjq.parseJqFilter ".fieldName" ~?= Right (JqField "fieldName" JqNil)
     , "jqFilterParser test 4" ~: Hjq.parseJqFilter ".[0].fieldName" ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
     , "jqFilterParser test 5" ~: Hjq.parseJqFilter ".fieldName[0]" ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
+    ]
+
+jqFilterParserSpacesTest :: Test
+jqFilterParserSpacesTest = TestList
+    [ "jqFilterParser test 1" ~: Hjq.parseJqFilter " . " ~?= Right JqNil
+    , "jqFilterParser test 2" ~: Hjq.parseJqFilter " . [ 0 ] " ~?= Right (JqIndex 0 JqNil)
+    , "jqFilterParser test 3" ~: Hjq.parseJqFilter " . fieldName " ~?= Right (JqField "fieldName" JqNil)
+    , "jqFilterParser test 4" ~: Hjq.parseJqFilter " . [ 0 ] . fieldName " ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
+    , "jqFilterParser test 5" ~: Hjq.parseJqFilter " . fieldName [ 0 ] " ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
     ]
